ab53524e030cbaed65dfc772c0ad2beb15b15f9f
Khangnekk <contact.khalgfk@gmail.com>
2023-09-29 16:33:52 +0700

    Init Map2

---
+++ DK_Game/Assets/Scripts/Demo/FollowPlayer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = player.transform.position + new Vector3(0, 1, -5);
+    }
+}
