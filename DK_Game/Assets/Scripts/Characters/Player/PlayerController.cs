0e617a2dc8070200e54112a87a7330a9d851b70f
NVA <vieetanhnguyeenx@gmail.com>
2023-10-09 23:09:33 +0700

    Update Player Movement

--- DK_Game/Assets/Scripts/Characters/Player/PlayerController.cs
+++ DK_Game/Assets/Scripts/Characters/Player/PlayerController.cs
@@ -1,37 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D)   )]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Movment Variables")]
     [SerializeField] public float walkSpeed = 5f;
 
     private Rigidbody2D rb;
+    private Animator animator;
 
     // Read from Player Input Action value -1 0 1
     private Vector2 moveInput;
 
-    // 
-    public bool IsMoving { get; private set; }
+    private bool _isMoving = false;
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
