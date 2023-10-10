bd3522cb5b3953c03dde2b81ee9abc624c6e7dc8
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-10-10 11:16:53 +0700

    switch scene

---
+++ DK_Game/Assets/Scripts/Map/Switch Scenes.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float delay;
+    public string nameScene;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.SetActive(false);
+            ModeSelect();
+        }
+    }
+    public void ModeSelect()
+    {
+        StartCoroutine((IEnumerator)LoadAfterDelay());
+    }
+    IEnumerable LoadAfterDelay()
+    {
+        yield return new WaitForSeconds(delay); 
+        SceneManager.LoadScene(nameScene);
+    }
+}
