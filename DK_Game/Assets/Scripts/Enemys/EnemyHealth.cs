7accba76ea30ad905d601f4a4564d03a94113ca7
Khangnekk <contact.khalgfk@gmail.com>
2023-10-24 11:01:32 +0700

    add Blade Master (enemy prefab)

---
+++ DK_Game/Assets/Scripts/Enemys/EnemyHealth.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int health = 500;
+
+    public GameObject deathEffect;
+
+    public bool isInvulnerable = false;
+
+    public void TakeDamage(int damage)
+    {
+        if (isInvulnerable)
+            return;
+
+        health -= damage;
+
+        if (health <= 200)
+        {
+            GetComponent<Animator>().SetBool("IsEnraged", true);
+        }
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ DK_Game/Assets/Scripts/Enemys/Enemy_Fight.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class Enemy_Fight : MonoBehaviour
+{
+    public int attackDamage = 20;
+    public int enragedAttackDamage = 40;
+
+    public Vector3 attackOffset;
+    public float attackRange = 1f;
+    public LayerMask attackMask;
+
+    public void Attack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null)
+        {
+            //colInfo.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+            Debug.Log("Send damage to player");
+        }
+    }
+
+    public void EnragedAttack()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null)
+        {
+            //colInfo.GetComponent<PlayerHealth>().TakeDamage(enragedAttackDamage);
+            Debug.Log("Send damage to player (enrage)");
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Gizmos.DrawWireSphere(pos, attackRange);
+    }
+}
