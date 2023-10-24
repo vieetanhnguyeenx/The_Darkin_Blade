8cd5b55af850f616b0785b7a902a7d158a1a4a46
Khangnekk <contact.khalgfk@gmail.com>
2023-10-24 09:15:53 +0700

    draft boss

---
+++ DK_Game/Assets/Scripts/Bosses/Boss.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    Transform player;
+
+    public bool isFlipped = false;
+
+    public void LookAtPlayer()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
+}
---
+++ DK_Game/Assets/Scripts/StateMachine/Boss_Jump.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Boss_Jump : StateMachineBehaviour
+{
+    float speed = 3f;
+    Transform player;
+    Rigidbody2D rb;
+    Boss boss;
+    //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+        boss = animator.GetComponent<Boss>();
+        Debug.Log("OnStateEnter");
+    }
+
+    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        boss.LookAtPlayer();
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+        Debug.Log(newPos);
+    }
+
+    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("OnStateExit");
+    }
+}
