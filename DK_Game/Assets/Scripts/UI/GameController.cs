19015e32306fcfbfe3e7903e336b1a955ffe7b4d
Khangnekk <contact.khalgfk@gmail.com>
2023-11-03 06:26:46 +0700

    GameController prefab

---
+++ DK_Game/Assets/Scripts/UI/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.UI
+{
+    internal class GameController : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject Panel;
+        private void Start()
+        {
+            Panel.SetActive(false);
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                Panel.SetActive(true);
+            }
+        }
+        public void btnYesClicked()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        public void btnNoClicked()
+        {
+            Panel.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
