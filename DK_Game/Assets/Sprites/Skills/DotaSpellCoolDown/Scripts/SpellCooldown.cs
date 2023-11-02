ff12a3f3fd1afc14130db83de2389669b3dfa8e3
Khangnekk <contact.khalgfk@gmail.com>
2023-11-02 20:53:15 +0700

    UI Skills

---
+++ DK_Game/Assets/Sprites/Skills/DotaSpellCoolDown/Scripts/SpellCooldown.cs
@@ -0,0 +1,83 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellCooldown : MonoBehaviour
+{
+    [Header("UI items for Spell Cooldown")]
+    [Tooltip("Tooltip example")]
+    [SerializeField]
+    private Image imageCooldown;
+    [SerializeField]
+    private TMP_Text textCooldown;
+    [SerializeField]
+    private Image imageEdge;
+    [SerializeField]
+    private KeyCode Key;
+    [SerializeField]
+    private float cooldownTime;
+
+    //variable for looking after the cooldown
+    private bool isCoolDown = false;
+    private float cooldownTimer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textCooldown.gameObject.SetActive(false);
+        imageEdge.gameObject.SetActive(false);
+        imageCooldown.fillAmount = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(Key))
+        {
+            UseSpell();
+        }
+
+        if (isCoolDown)
+        {
+            ApplyCooldown();
+        }
+    }
+
+    void ApplyCooldown()
+    {
+        cooldownTimer -= Time.deltaTime;
+        if (cooldownTimer < 0.0f)
+        {
+            isCoolDown = false;
+            textCooldown.gameObject.SetActive(false);
+            imageEdge.gameObject.SetActive(false);
+            imageCooldown.fillAmount = 0.0f;
+        }
+        else
+        {
+            textCooldown.text = Mathf.RoundToInt(cooldownTimer).ToString();
+            imageCooldown.fillAmount = cooldownTimer / cooldownTime;
+
+            imageEdge.transform.localEulerAngles = new Vector3(0, 0, 360.0f * (cooldownTimer / cooldownTime));
+        }
+
+    }
+
+    public bool UseSpell()
+    {
+        if (isCoolDown)
+        {
+            return false;
+        }
+        else
+        {
+            isCoolDown = true;
+            textCooldown.gameObject.SetActive(true);
+            cooldownTimer = cooldownTime;
+            textCooldown.text = Mathf.RoundToInt(cooldownTimer).ToString();
+            imageCooldown.fillAmount = 1.0f;
+
+            imageEdge.gameObject.SetActive(true);
+            return true;
+        }
+    }
+}
